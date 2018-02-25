47fecb3e1d3944acad83bd749ba639064a4e6dcb
Valera Rozuvan <valera.rozuvan@gmail.com>
2018-02-25 11:57:39 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,22 @@
+var ip = require('ip');
+var fs = require('fs');
+var PeerServer = require('peer').PeerServer;
+
+var port = 9000;
+var server = new PeerServer({
+  port: port,
+  ssl: {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.crt')
+  }
+});
+
+server.on('connection', function (id) {
+  console.log('new connection with id ' + id);
+});
+
+server.on('disconnect', function (id) {
+  console.log('disconnect with id ' + id);
+});
+
+console.log('peer server running on ' + ip.address() + ':' + port);
